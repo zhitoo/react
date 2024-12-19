2c151a83608c1f76df2a6d99e6ccbea9bd311cc0
Hossein Shafee <zhitoooo@gmail.com>
2024-12-19 22:10:51 +0330

    update header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
+
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <header>
-      <h1>My Website</h1>
-      <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
+    <header className="flex flex-row items-center justify-between sm:justify-around p-2 border-b-2 bg-gray-100">
+      <a
+        href="/"
+        className="flex items-center h-10 px-10 bg-gradient-to-r from-gray-900 via-gray-600 to-gray-500 rounded-tl-full rounded-br-full font-bold uppercase italic text-white hover:opacity-90"
+      >
+        AIZOOME
+      </a>
+      <nav className="hidden sm:flex justify-between items-center gap-4 font-semibold">
+        <a href="#" className="hover:text-gray-500">
+          Home
+        </a>
+        <a href="#" className="hover:text-gray-500">
+          About
+        </a>
+        <a href="#" className="hover:text-gray-500">
+          Contact
+        </a>
       </nav>
-      <hr></hr>
+      <nav className="sm:hidden flex flex-col items-end gap-1 font-semibold">
+        <button
+          className="sm:hidden font-bold text-xl hover:text-gray-500"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          {showMenu ? "Menu" : "X"}
+        </button>
+        {showMenu && (
+          <>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </>
+        )}
+      </nav>
     </header>
   );
 }
